ce9e84c38000ddc747a38f2db6d11abdc96232dd
Andro <androbienaime@gmail.com>
2021-04-10 22:27:58 -0400

    Adding licence

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -30,7 +30,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => 'cb080b4c72c304e0affa49181a283bb6ad75c634',
+    'reference' => '1c1a7e5c1dddcd9836a9a6c0935ef8e2697df05c',
     'name' => 'lestruviens/sms',
   ),
   'versions' => 
@@ -96,7 +96,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => 'cb080b4c72c304e0affa49181a283bb6ad75c634',
+      'reference' => '1c1a7e5c1dddcd9836a9a6c0935ef8e2697df05c',
     ),
     'mtdowling/jmespath.php' => 
     array (
